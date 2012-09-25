99d3398adcc90312999f53c4102327daf2ebe76b
Charlie Curtsinger <charlie@cs.umass.edu>
2012-09-25 11:58:41 -0400

    Incremental build support.  Added a default setting for CPU=i386 for now.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Probabilistic Race Tolerance
 
 ## Building
-Proto currently only supports 32-bit x86 on Linux.  To build the 32-bit library and tests, invoke `make CPU=i386` at the project root.
+Proto currently only supports 32-bit x86 on Linux.  By default, the makefile will assume CPU=i386.  This can be overridden on the command line by running `make CPU=x86_64`.
 
 ## Testing
-Running tests will first build Proto.  To run tests, type `make test CPU=i386` at the root of the project.
+Running tests will first build Proto.  To run tests, type `make test` at the root of the project.
 
 To add new tests, duplicate the Makefile and source structure from tests/sample.  Add the new test directory name to the `DIRS` variable in tests/Makefile.
--- common.mk
+++ common.mk
@@ -1,6 +1,8 @@
 # Get the current OS and architecture
 OS ?= $(shell uname -s)
-CPU ?= $(shell uname -m)
+# Default to i386 for now
+#####CPU ?= $(shell uname -m)
+CPU ?= i386
 
 # Set the default compilers and flags
 CC = gcc
@@ -37,6 +39,7 @@ endif
 
 # Set the default source and include files with wildcards
 SRCS ?= $(wildcard *.c) $(wildcard *.cpp) $(wildcard *.cc) $(wildcard *.C)
+OBJS ?= $(addprefix obj/, $(patsubst %.c, %.o, $(patsubst %.cpp, %.o, $(patsubst %.cc, %.o, $(patsubst %.C, %.o, $(SRCS))))))
 INCLUDES ?= $(wildcard *.h) $(wildcard *.hpp) $(wildcard *.hh) $(wildcard *.H) $(wildcard $(addsuffix /*.h, $(INCLUDE_DIRS))) $(wildcard $(addsuffix /*.hpp, $(INCLUDE_DIRS))) $(wildcard $(addsuffix /*.hh, $(INCLUDE_DIRS))) $(wildcard $(addsuffix /*.H, $(INCLUDE_DIRS)))
 
 # Generate flags to link required libraries and get includes
@@ -45,11 +48,27 @@ INCFLAGS = $(addprefix -I, $(INCLUDE_DIRS))
 
 build:: $(TARGETS)
 
-$(filter %.$(SHLIB_SUFFIX), $(TARGETS)):: $(SRCS) $(INCLUDES)
-	$(CXXLIB) $(CXXFLAGS) $(INCFLAGS) $(SRCS) -o $@ $(LIBFLAGS)
+obj/%.o: %.c
+	@mkdir -p obj
+	$(CC) $(CFLAGS) -c $< -o $@
 
-$(filter-out %.$(SHLIB_SUFFIX), $(TARGETS)):: $(SRCS) $(INCLUDES)
-	$(CXX) $(CXXFLAGS) $(INCFLAGS) $(SRCS) -o $@ $(LIBFLAGS)
+obj/%.o: %.cpp
+	@mkdir -p obj
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+	
+obj/%.o: %.cc
+	@mkdir -p obj
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+	
+obj/%.o: %.C
+	@mkdir -p obj
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+$(filter %.$(SHLIB_SUFFIX), $(TARGETS)):: $(OBJS) $(INCLUDES)
+	$(CXXLIB) $(CXXFLAGS) $(INCFLAGS) $(OBJS) -o $@ $(LIBFLAGS)
+
+$(filter-out %.$(SHLIB_SUFFIX), $(TARGETS)):: $(OBJS) $(INCLUDES)
+	$(CXX) $(CXXFLAGS) $(INCFLAGS) $(OBJS) -o $@ $(LIBFLAGS)
 
 $(RECURSIVE_TARGETS)::
 	@for dir in $(DIRS); do \
